82b1e3dd2129f1b38e0666519a359b7a415f9b87
EEEErfan <139115035+EEEErfan@users.noreply.github.com>
2024-11-07 11:31:56 +0330

    Fix Chat Object

--- src/Objects/Chat.php
+++ src/Objects/Chat.php
@@ -13,8 +13,8 @@ use EFive\Bale\Objects\ChatPhoto;
  * @property string $type Type of chat, can be either 'private', 'group' or 'channel'.
  * @property string|null $title (Optional). Title, for channels and group chats.
  * @property string|null $username (Optional). Username, for private chats and channels if available
- * @property string|null $firstName (Optional). First name of the other party in a private chat
- * @property string|null $lastName (Optional). Last name of the other party in a private chat
+ * @property string|null $first_name (Optional). First name of the other party in a private chat
+ * @property string|null $last_name (Optional). Last name of the other party in a private chat
  * @property photo|null $photo (Optional). Chat photo. Returned only in getChat.
  */
 class Chat extends BaseObject
