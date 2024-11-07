cb178c0a5851a78441c3e601b4cccf24da370e79
EEEErfan <139115035+EEEErfan@users.noreply.github.com>
2024-11-07 12:11:27 +0330

    Update BaseObject and add BaleObject

---
+++ src/Objects/BaleObject.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EFive\Bale\Objects;
+
+/**
+ * Class Object.
+ */
+class BaleObject extends BaseObject
+{
+    /**
+     * Property relations.
+     */
+    public function relations(): array
+    {
+        return [];
+    }
+}
