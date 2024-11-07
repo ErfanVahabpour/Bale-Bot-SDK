81a89a64f223335c77d35f1ae91940e49598dae3
EEEErfan <139115035+EEEErfan@users.noreply.github.com>
2024-11-07 11:31:28 +0330

    Fix User Object

--- src/Objects/User.php
+++ src/Objects/User.php
@@ -8,11 +8,11 @@ namespace EFive\Bale\Objects;
  * @link https://docs.bale.ai/#user
  *
  * @property int $id Unique identifier for this user or bot.
- * @property bool $isBot True, if this user is a bot
- * @property string $firstName User's or bot's first name.
- * @property string|null $lastName (Optional). User's or bot's last name.
+ * @property bool $is_bot True, if this user is a bot
+ * @property string $first_name User's or bot's first name.
+ * @property string|null $last_name (Optional). User's or bot's last name.
  * @property string|null $username (Optional). User's or bot's username.
- * @property string|null $languageCode (Optional). IETF language tag of the user's language
+ * @property string|null $language_code (Optional). IETF language tag of the user's language
  */
 class User extends BaseObject
 {
